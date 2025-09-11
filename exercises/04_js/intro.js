c2c1c5e271ad95c0c483cd9b6f2ba3aa12148ed9
Charlotte Baker <crb27@email.sc.edu>
2025-09-11 12:16:55 -0400

    Added note on main page CSS about BEM

--- exercises/04_js/intro.js
+++ exercises/04_js/intro.js
@@ -1,6 +1,32 @@
-function sayHello() {
-    console.log('Hello, World!');
+/*
+const sayHello = () => {
+    console.log("Hello World");
 }
-const btnClickMe = document.getElementById('btn-click-me');
-console.log(btnClickMe);
-btnClickMe.onclick = sayHello();
+
+document.getElementById("btn-click-me").onclick = sayHello;
+*/
+
+document.getElementById("btn-click-me").onclick = () => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    //document.getElementById("btn-click-me").classList.add("clicked");
+    event.currentTarget.classList.add("clicked"); //current target is the button that was clicked
+
+};
+
+document.getElementById("happy-button").onclick = () => {
+    document.getElementById("p-happy").innerHTML = "Have a great day!";
+    event.currentTarget.classList.add("happy");
+};
+
+document.getElementById("sad-button").onclick = () => {
+    document.getElementById("p-sad").innerHTML = "Cheer up!";
+    event.currentTarget.classList.add("sad");
+};
+
+document.getElementById("clear-button").onclick = () => {
+    document.getElementById("p-happy").innerHTML = "";
+    document.getElementById("p-sad").innerHTML = "";
+    document.getElementById("happy-button").classList.remove("happy");
+    document.getElementById("sad-button").classList.remove("sad");
+    event.currentTarget.classList.add("cleared");
+}
