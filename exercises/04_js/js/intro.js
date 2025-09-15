70698cae10fea3513840d9d01c011c8c91bd4e14
Charlotte Baker <crb27@email.sc.edu>
2025-09-14 20:31:10 -0400

    HTML and CSS pages start

--- exercises/04_js/js/intro.js
+++ exercises/04_js/js/intro.js
@@ -6,28 +6,34 @@ const sayHello = () => {
 document.getElementById("btn-click-me").onclick = sayHello;
 */
 
-document.getElementById("btn-click-me").onclick = () => {
+document.getElementById("btn-click-me").onclick = (event) => {
     document.getElementById("p-welcome").innerHTML = "Hello World";
-    //document.getElementById("btn-click-me").classList.add("clicked");
-    event.currentTarget.classList.add("clicked"); //current target is the button that was clicked
-
+    event.currentTarget.classList.add("clicked");   //current target is the button that was clicked
 };
 
-document.getElementById("happy-button").onclick = () => {
-    const moodDisplay = document.getElementById("mood-display");
-    moodDisplay.innerHTML = "Yay! I'm so happy!";
-    moodDisplay.classList.add("happy");
-};
-
-document.getElementById("sad-button").onclick = () => {
-    document.getElementById("mood-display").innerHTML = "Cheer up!";
-    event.currentTarget.classList.add("sad");
-};
-
-document.getElementById("clear-button").onclick = () => {
-    document.getElementById("mood-display").innerHTML = "";
-    document.getElementById("mood-display").innerHTML = "";
-    document.getElementById("happy-button").classList.remove("happy");
-    document.getElementById("sad-button").classList.remove("sad");
-    event.currentTarget.classList.add("cleared");
+document.getElementById("btn-happy").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "YAY";
+    pFeeling.classList.add("happy");
+    pFeeling.classList.remove("sad");
 }
+
+document.getElementById("btn-sad").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "Nay";
+    pFeeling.classList.add("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("btn-clear").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "";
+    pFeeling.classList.remove("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
+    document.getElementById("img-emotion").classList.remove("hidden");
+};
